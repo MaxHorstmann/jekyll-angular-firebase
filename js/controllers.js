88fad8ae847487625cf1d2e90b34c9378796bf98
Max Horstmann <max.horstmann@gmail.com>
2014-08-15 22:57:44 -0400

    customers

--- js/controllers.js
+++ js/controllers.js
@@ -12,4 +12,17 @@ jafApp.controller('jafController', ["$scope", "$firebase", function ($scope, $fi
   	var syncObject = sync.$asObject();
   	syncObject.$bindTo($scope, "data");
 
+  	$scope.addCustomer = function() {
+  		if (!$scope.data) $scope.data = {};
+  		if (!$scope.data.customers) $scope.data.customers = [];
+
+  		$scope.data.customers.push({ id: Math.floor((Math.random() * 100) + 1), name: 'john' });
+  	};
+
+  	$scope.removeCustomer = function(id) {
+  		for(var i = 0; i<$scope.data.customers.length; i++){
+			if ($scope.data.customers[i].id == id) $scope.data.customers.splice(i, 1);
+		}
+  	}
+
 }]);
