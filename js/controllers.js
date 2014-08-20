01c85a8dab3760bc73b05a8e26ac31892c9778d0
Max Horstmann <mhorstmann@stackoverflow.com>
2014-08-20 16:55:15 -0400

    login/logout

--- js/controllers.js
+++ js/controllers.js
@@ -6,8 +6,33 @@ var jafApp = angular.module('jafApp', ['firebase'],
 
 jafApp.controller('jafController', ["$scope", "$firebase", function ($scope, $firebase) {
 
-	var firebase_url = document.getElementById('data-firebase-url').getAttribute('data-firebase-url');
-	var sync = $firebase(new Firebase(firebase_url));
+  	var firebase_url = document.getElementById('data-firebase-url').getAttribute('data-firebase-url');
+
+    var firebaseRef = new Firebase(firebase_url);
+
+    $scope.user = {};
+    
+    var firebaseAuthClient = new FirebaseSimpleLogin(firebaseRef, function(error, user) {
+      if (error) {
+        alert(error);
+      } else if (user) {
+        $scope.user = user;
+      } else {
+        $scope.user = { };
+      }
+      $scope.$apply();
+    });
+
+    $scope.login = function() {
+      firebaseAuthClient.login('password', { email: 'testuser@example.com',  password: 'password' });
+    };
+
+    $scope.logout = function() {
+      firebaseAuthClient.logout();;
+    };
+
+
+  	var sync = $firebase(firebaseRef);
 
   	var syncObject = sync.$asArray();
   	//syncObject.$bindTo($scope, "customers");
@@ -21,8 +46,9 @@ jafApp.controller('jafController', ["$scope", "$firebase", function ($scope, $fi
 
   	$scope.removeCustomer = function(id) {
   		for(var i = 0; i<$scope.customers.length; i++){
-			if ($scope.customers[i].id == id) $scope.customers.$remove(i);
-		}
+  		if ($scope.customers[i].id == id) $scope.customers.$remove(i);
   	}
+
+  }
 
 }]);
