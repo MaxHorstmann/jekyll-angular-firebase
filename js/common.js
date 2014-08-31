d0ee35c99e584bec9e3e9fd936204022bf52125e
Max Horstmann <max.horstmann@gmail.com>
2014-08-30 21:54:15 -0400

    split across 2 files

--- js/common.js
+++ js/common.js
@@ -35,38 +35,3 @@ jafApp.controller('authController', ['$scope', 'firebaseConnection', function($s
       firebaseAuthClient.logout();;
     };
 }]);
-
-jafApp.controller('customersController', ["$scope", "$firebase", 'firebaseConnection',
-  function ($scope, $firebase, firebaseConnection) {
-
-    // subscript to user, only show write UI if authenticated
-    $scope.$watch(function() { return firebaseConnection.user; }, function(user) {
-      $scope.user = user;
-      $scope.$apply();
-    });
-
-    $scope.filter = '';
-
-  	$scope.customers = $firebase(new Firebase(firebaseConnection.firebase_url + '/customers')).$asArray();
-    $scope.newCustomer = '';
-
-    var firebaseCustomerIdSequenceRef = new Firebase(firebaseConnection.firebase_url + '/customerIdSequence');
-  	$scope.addCustomer = function() {  		
-      firebaseCustomerIdSequenceRef.transaction(function(id) {
-        return id+1;
-      }, function(error, committed, snapshot) {
-        if (committed) {
-          $scope.customers.$add({ id: snapshot.val(), name: $scope.newCustomer });
-          $scope.newCustomer = '';
-        }
-      });
-  	};
-
-  	$scope.removeCustomer = function(id) {
-  		for(var i = 0; i<$scope.customers.length; i++){
-  		if ($scope.customers[i].id == id) $scope.customers.$remove(i);
-  	}
-
-  }
-
-}]);
